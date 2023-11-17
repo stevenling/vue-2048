885c4e6204240b377351e2a5c50ac33b0fd6045f
yunhu <703019049@qq.com>
2023-11-17 19:09:21 +0800

    feat: add server url

--- src/http/networkConfig.js
+++ src/http/networkConfig.js
@@ -1,6 +1,6 @@
 export const networkConfig = {
     // 远程
-    serverUrl: 'http://yunhu.wiki:2048/api',
+    serverUrl: 'https://2048.yunhu.wiki/api',
     // serverUrl: 'http://localhost:8090',
     requestTimeout: 20000
 }
